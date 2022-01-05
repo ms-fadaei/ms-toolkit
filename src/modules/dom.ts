2d172fdc2cf66f1e857d6af00ecd66afcf158d9a
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-01-05 08:16:22 +0330

    feat: add `imageToBase64`

--- src/modules/dom.ts
+++ src/modules/dom.ts
@@ -80,3 +80,25 @@ function attachEvents(
 
   events.forEach((name) => el.addEventListener(name, createEventHandler(name), eventOptions));
 }
+
+export function imageToBase64(src: string, format?: string, quality?: number): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      const ctx = canvas.getContext('2d');
+      if (!ctx) {
+        return reject(new Error('Canvas is not supported'));
+      }
+
+      ctx.drawImage(img, 0, 0);
+      resolve(canvas.toDataURL(format, quality));
+    };
+
+    img.onerror = reject;
+    img.src = src;
+  });
+}
