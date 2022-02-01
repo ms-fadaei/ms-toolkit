d81e1f8e9394361ea51564aff2fada46d09e4a26
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-02-01 10:11:14 +0330

    feat: add `scrollWindowToSection` helper function

--- src/modules/dom.ts
+++ src/modules/dom.ts
@@ -132,3 +132,33 @@ export function getElementPosition(el: HTMLElement) {
     scrollY: elementRect.top + window.scrollY,
   };
 }
+
+export function scrollWindowToSection(
+  el?: HTMLElement | string,
+  options?: { marginTop: number },
+): void {
+  // eslint-disable-next-line prettier/prettier
+  const element = (typeof el === 'string' ? document.querySelector(el) : el) || window.document.body;
+  const { marginTop = 0 } = options || {};
+  const supportsNativeSmoothScroll = 'scrollBehavior' in document.documentElement.style;
+
+  let offsetTop = getElementPosition(element as HTMLElement).scrollY - marginTop;
+  offsetTop = offsetTop < 0 ? 0 : offsetTop;
+
+  if (supportsNativeSmoothScroll) {
+    // cross browser smooth scroll
+    if (offsetTop === 0) {
+      window.document.body.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    } else {
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  } else {
+    window.scrollTo(0, offsetTop);
+  }
+}
