cf9a2b14e91fba20296704dba7c5a47638b8dcc7
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-02-01 09:45:58 +0330

    feat: add `getElementPosition` helper function

--- src/modules/dom.ts
+++ src/modules/dom.ts
@@ -115,3 +115,20 @@ export function getScrollableParent(el: HTMLElement): HTMLElement {
 
   return document.documentElement;
 }
+
+export function getElementPosition(el: HTMLElement) {
+  if (!el || !(el instanceof HTMLElement)) {
+    throw new Error('Element is not defined');
+  }
+
+  const elementRect = el.getBoundingClientRect();
+
+  return {
+    width: elementRect.width,
+    height: elementRect.height,
+    left: elementRect.left,
+    top: elementRect.top,
+    scrollX: elementRect.left + window.scrollX,
+    scrollY: elementRect.top + window.scrollY,
+  };
+}
