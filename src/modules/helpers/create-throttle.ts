65252722b80a9ba2d9c94f856c4e0ce730e5b797
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-02-02 14:08:08 +0330

    feat: add `createThrottle` helper function

---
+++ src/modules/helpers/create-throttle.ts
@@ -0,0 +1,35 @@
+type FunctionType = (...args: unknown[]) => void;
+
+export default function createThrottle(
+  this: unknown,
+  cb: FunctionType,
+  delay = -1,
+  leading = false,
+): FunctionType {
+  // throttle flag to prevent run
+  let isThrottling = false;
+
+  return (...args: unknown[]): void => {
+    if (!isThrottling) {
+      const boundedCb = cb.bind(this, ...args);
+
+      const doAfterTimeout = (): void => {
+        if (!leading) boundedCb();
+        isThrottling = false;
+      };
+
+      if (delay < 0) {
+        // use requestAnimationFrame for negative delay
+        requestAnimationFrame(doAfterTimeout);
+      } else {
+        // use setTimeout for positive delay
+        setTimeout(doAfterTimeout, delay);
+      }
+
+      isThrottling = true;
+
+      // call immediately if leading is enabled
+      leading && boundedCb();
+    }
+  };
+}
