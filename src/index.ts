26ba3afd26726b823a5630be6b925ec5e69bb750
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-02-02 21:28:13 +0330

    feat: add `createDebounce` and `createThrottle` to the lib

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './modules/patterns';
 export * from './modules/text';
 export * from './modules/dom';
+export * from './modules/helpers';
---
+++ src/modules/helpers/index.ts
@@ -0,0 +1,2 @@
+export { default as createDebounce } from './create-debounce';
+export { default as createThrottle } from './create-throttle';
